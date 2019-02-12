56e8339eda0ba7b418fc1f756da56154a82562b2
Pavel Ivanov <tyrension@gmail.com>
2019-02-12 17:48:52 +0700

    Setup Eureka Server

--- eurekaserver/src/main/java/io/microdrive/eurekaserver/Application.java
+++ eurekaserver/src/main/java/io/microdrive/eurekaserver/Application.java
@@ -1,12 +1,10 @@
-package io.microdrive.app;
+package io.microdrive.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@RefreshScope
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
