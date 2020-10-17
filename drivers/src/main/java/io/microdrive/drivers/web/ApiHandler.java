430b86df6820a051243f3313193ef3de26b28907
Pavel Ivanov <ivanvpvl@gmail.com>
2020-10-17 23:09:09 +0700

    Return error if no free drivers found

--- drivers/src/main/java/io/microdrive/drivers/web/ApiHandler.java
+++ drivers/src/main/java/io/microdrive/drivers/web/ApiHandler.java
@@ -1,16 +1,19 @@
 package io.microdrive.drivers.web;
 
-import io.microdrive.core.dto.drivers.DriverResponse;
 import io.microdrive.core.dto.drivers.ReleaseRequest;
+import io.microdrive.core.dto.errors.ResponseError;
+import io.microdrive.drivers.errors.FreeDriverNotFoundError;
 import io.microdrive.drivers.service.DriverService;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.web.reactive.function.server.ServerResponse.status;
 
 @Component
 @RequiredArgsConstructor
@@ -18,8 +21,15 @@ class ApiHandler {
     private final DriverService driverService;
 
     Mono<ServerResponse> request(ServerRequest request) {
-        val account = driverService.request();
-        return ok().body(account, DriverResponse.class);
+        return driverService.request()
+            .flatMap(driver -> ok().bodyValue(driver))
+            .onErrorResume(error -> {
+                if (error instanceof FreeDriverNotFoundError) {
+                    return status(HttpStatus.NOT_FOUND).bodyValue(new ResponseError("Free drivers not found."));
+                }
+
+                return status(HttpStatus.BAD_REQUEST).bodyValue(new ResponseError(error.getMessage()));
+            });
     }
 
     Mono<ServerResponse> release(ServerRequest request) {
