9f3276a8537e5cea3970ef38f1079980751f7a05
Pavel Ivanov <ivanvpvl@gmail.com>
2021-06-13 11:34:54 +0700

    Remove testing module

--- core/src/main/java/io/microdrive/core/errors/AccountNotFoundException.java
+++ core/src/main/java/io/microdrive/core/errors/AccountNotFoundException.java
@@ -1,6 +1,6 @@
 package io.microdrive.core.errors;
 
-public class AccountNotFoundException extends RuntimeException {
+public class AccountNotFoundException extends ResourceNotFoundException {
     public AccountNotFoundException(String phoneNumber) {
         super(String.format("Account with phoneNumber='%s' not found", phoneNumber));
     }
